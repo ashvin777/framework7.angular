e124c1950cec98a235f7ebf94600a4d550c9d9ab
ashvin <ashvin.suthar777@gmail.com>
2015-11-22 23:16:05 +0530

    adding new work

--- Framework7-modified/js/app.js
+++ Framework7-modified/js/app.js
@@ -1,7 +1,7 @@
 angular.module('starter', ["framework7.angular","RootCtrl"])
 
 .run(["$AppBootloader",function($AppBootloader){
-    $AppBootloader.registerAngularCompiler();
+    //$AppBootloader.registerAngularCompiler();
 }])
 
 .config([function(){
