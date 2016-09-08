b3d1bf4a601e7c036c2e7fa24474e45ecd95e194
Ashvin Kumar Suthar <ashvin777@users.noreply.github.com>
2016-09-08 20:18:03 +0530

    Update framework7.angular.hook.js

--- kitchen-sink-material/js/framework7.angular.hook.js
+++ kitchen-sink-material/js/framework7.angular.hook.js
@@ -1,25 +1,33 @@
 Framework7.prototype.plugins.angular = function(app, params) {
-    function compile(newPage) {
-        try {
-            var $page = $(newPage);
-            var injector = angular.element("[ng-app]").injector();
-            var $compile = injector.get("$compile");
-            var $timeout = injector.get("$timeout");
-            var $scope = injector.get("$rootScope");
-            $scope = $scope.$$childHead;
-            $timeout(function() {
-                $compile($page)($scope);
-            })
-        } catch (e) {
-            //console.error("Some Error Occured While Compiling The Template", e);
-        }
+  function compile(newPage) {
+    try {
+      var $page = $(newPage);
+      var injector = angular.element("[ng-app]").injector();
+      var $compile = injector.get("$compile");
+      var $timeout = injector.get("$timeout");
+      var $scope = injector.get("$rootScope");
+      $scope = $scope.$$childHead;
+      $timeout(function() {
+        $compile($page)($scope);
+      })
+    } catch (e) {
+      //console.error("Some Error Occured While Compiling The Template", e);
     }
+  }
 
-    return {
-        hooks: {
-            pageInit: function(pageData) {
-                compile(pageData.container);
-            }
-        }
+  function removeOldPage(pageData){
+    var $oldPage =  $(".page").not( $(pageData.container));
+    $oldPage.remove();
+  }
+
+  return {
+    hooks: {
+      pageInit: function(pageData) {
+        compile(pageData.container);
+      },
+      pageAfterAnimation  : function(pageData){
+        removeOldPage(pageData);
+      }
     }
+  }
 };
